1693b363cc01b8e9ca73922b4e7d6eab19721990
sohamderoy <sohamderoy.iitk@gmail.com>
2020-04-10 01:42:05 +0530

    Shortened the code

--- Week 1/pa1_2.py
+++ Week 1/pa1_2.py
@@ -1,11 +1,4 @@
 no=int(input())
-a=input()
-b=a.split()
-l=[]
-m=[]
-#print(b)
-#print(type(b[0]))
-for i in b:
-    m.append(int(i))
+m=[int(x) for x in input().split()]
 m.sort()
 print(m[-1]*m[-2])
