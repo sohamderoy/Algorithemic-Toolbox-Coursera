af240da2eebf1965d9183d8141d13a39d4520d90
sohamderoy <sohamderoy.iitk@gmail.com>
2020-04-10 01:20:45 +0530

    time complexity resolved for pa1_2.py (10.02/5.00 to 0.14/5.00)

--- Week 1/pa1_2.py
+++ Week 1/pa1_2.py
@@ -7,14 +7,5 @@ m=[]
 #print(type(b[0]))
 for i in b:
     m.append(int(i))
-#print(type(m[0]))
-count=0
-for i in range(len(m)):
-    for j in range (i+1, len(m)):
-        k=m[i]*m[j]
-        count=count+1
-        l.append(k)
-#print(l,count)
-l.sort()
-print(l[-1])
-
+m.sort()
+print(m[-1]*m[-2])
