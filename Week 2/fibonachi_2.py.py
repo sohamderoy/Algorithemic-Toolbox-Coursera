dd385c46e28bbfb76032ff88fbb20249d90301d6
sohamderoy <sohamderoy.iitk@gmail.com>
2020-04-10 15:14:32 +0530

    wrote comment for fibonachi_1 and slow version of fibonachi_2

--- Week 2/fibonachi_2.py.py
+++ Week 2/fibonachi_2.py.py
@@ -1,4 +1,4 @@
-# to find ou thte last no of a fibonachi series
+#to find the last digit of the last no of a fibonachi series [slow version]
 n=int(input())
 sum=1
 f=[0,1,0]
@@ -6,4 +6,4 @@ for i in range(n):
     f[2]=(f[1]+f[0])
     f[0]=f[1]
     f[1]=f[2]
-print(f[0])
+print(f[0]%10)
