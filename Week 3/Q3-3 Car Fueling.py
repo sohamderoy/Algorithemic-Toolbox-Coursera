9f5a5a91372f9acf60d853ab51c225dc9bda2ba0
sohamderoy <sohamderoy.iitk@gmail.com>
2020-04-14 22:40:01 +0530

    Q3 corrected

--- Week 3/Q3-3 Car Fueling.py
+++ Week 3/Q3-3 Car Fueling.py
@@ -1,45 +1,52 @@
-#To find the minimum no of refueling that need to be done to reach from point A to B
 import sys
 import math
 d=int(input())
 m=int(input())
 n=int(input())
 s=list(map(int, input().split()))
-count=0
+#####################################################
+#creating a helping list
+s2=[0]
+for _ in s:
+    s2.append(_)
+s2.append(d)
+refill=0
+#print(s2)
+#####################################################
+#some complicated stufffffffff
+#for refrence look at the previous commit for this section
 s1=s
 s1.append(d)
-#print(s1,1)
 s3=[0]
 s3.append(s1[0])
-for i in range (1,len(s1)):
-    s3.append(s1[i]-s1[i-1])
+for k in range (1,len(s1)):
+    s3.append(s1[k]-s1[k-1])
 #print(s2,2)
 #s2.append(-1)
 flag=False
-for i in s3:
-    if i>m:
+for g in s3:
+    if g>m:
         flag=True
         break
 #print(flag,3)
-refill=0
+
+######################################################
 if m>d:
     print(0)
     sys.exit()
 elif flag==True:
     print(-1)
     sys.exit()
-elif flag==False and m<d:
+else:
     start=0
-    i=start+2
-    while(i<=len(s2)):
-        if (sum(s2[start+1:i])<m):
-            i+=1
-            if (s2[i]==-1):
-                break
-        elif (sum(s2[start+1:i])>m):
-            start=i-2
+    i=start+1
+    while(i<=len(s2)-1):
+        if (s2[i]-s2[start])<=m:
+            try:
+                i=i+1
+            except:
+                sys.exit()
+        else:
+            start=i-1
             refill=refill+1
-            if s2[i]==-1:
-                break
-print(refill+)
-    
+    print(refill)
